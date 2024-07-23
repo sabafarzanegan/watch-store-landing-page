718dd7e6c730ee5ca363c4406026cc776d539c69
saba farzanegan <saba.far.99@gmail.com>
2024-07-23 16:36:35 +0330

    add darkmode to project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Hero() {
+  return <div className="h-[400px]">hero</div>;
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import logo from "../assets/toggle/logo (1).png";
 import { FaCaretDown } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import Darkmood from "./darkmood";
 const Menu = [
   {
     id: 1,
@@ -42,8 +44,8 @@ function Navbar() {
         </div>
         {/* menu section */}
         <div className="flex items-center justify-between gap-2">
-          <div>darkmood</div>
-          <ul className="flex items-center justify-center gap-4">
+          <Darkmood />
+          <ul className=" items-center justify-center gap-4 hidden md:flex">
             {Menu.map((item) => (
               <li>
                 <a
@@ -66,7 +68,7 @@ function Navbar() {
                   {DropdownLinks.map((item) => (
                     <li>
                       <a
-                        className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                        className="inline-block w-full rounded-md p-2 hover:bg-primary/20 dark:text-black"
                         href={item.link}>
                         {item.name}
                       </a>
@@ -76,6 +78,12 @@ function Navbar() {
               </div>
             </li>
           </ul>
+          {/* cart section */}
+          <div>
+            <button className="px-4 py-2 rounded-full hover:bg-gradient-to-r  hover:from-primary hover:to-secondary hover:scale-105 transition-all duration-200">
+              <FaCartShopping className="text-lg text-black dark:text-white drop-shadow-sm cursor-pointer" />
+            </button>
+          </div>
         </div>
       </div>
     </main>
